abd02eca7088c162753b849a3615e053705b6b84
Ramazan Subaşi <subasiramazan@gmail.com>
2014-04-09 11:14:08 +0300

    update

---
+++ main.c
@@ -0,0 +1,40 @@
+
+ #include <stdio.h>
+ #include <stdlib.h>
+#include <xc.h>
+//Configuration Settings
+
+// CONFIG1
+#pragma config FOSC = XT        // Oscillator Selection bits (XT oscillator)
+#pragma config WDTE = OFF       // Watchdog Timer Enable bit (WDT disabled)
+#pragma config PWRTE = OFF      // Power-up Timer Enable bit (PWRT disabled)
+#pragma config MCLRE = ON       // RA5/MCLR/VPP Pin Function Select bit (RA5/MCLR/VPP pin function is MCLR)
+#pragma config BOREN = OFF      // Brown-out Reset Enable bit (BOR disabled)
+#pragma config LVP = OFF        // Low-Voltage Programming Enable bit (RB3 is digital I/O, HV on MCLR must be used for programming)
+#pragma config CPD = OFF        // Data EE Memory Code Protection bit (Code protection off)
+#pragma config WRT = OFF        // Flash Program Memory Write Enable bits (Write protection off)
+#pragma config CCPMX = RB0      // CCP1 Pin Selection bit (CCP1 function on RB0)
+#pragma config CP = OFF         // Flash Program Memory Code Protection bit (Code protection off)
+
+// CONFIG2
+#pragma config FCMEN = OFF      // Fail-Safe Clock Monitor Enable bit (Fail-Safe Clock Monitor disabled)
+#pragma config IESO = OFF       // Internal External Switchover bit (Internal External Switchover mode disabled)
+
+#define _XTAL_FREQ 4000000
+#define LED PORTBbits.RB0
+
+
+
+
+
+
+void main(){
+ 
+TRISBbits.TRISB0=0;
+ while (1){
+    LED=~LED;
+    __delay_ms(500);
+          }
+
+ 
+ }
